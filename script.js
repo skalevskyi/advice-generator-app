b8061e917eaad7f3f5c4b69c6950419c16303b35
skalevskyi <sergeyskelky2901@gmail.com>
2024-10-02 23:20:01 +0200

    set timeout

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function startDiceAnimation() {
     setTimeout(() => {
       diceImage.classList.remove('animate');
       fetchAdvice();
-    }, 1000); 
+    }, 500); 
 }
 
 // Додаємо подію на кнопку: при натисканні отримуємо нову пораду
